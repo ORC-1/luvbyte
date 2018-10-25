17cf951f4e4f386ac9f6c4e22d8a5a6d8158fe01
ORC-1 <Rolandazim@gmail.com>
2018-10-25 18:10:37 +0100

    Login Debug

--- luvbyte/urls.py
+++ luvbyte/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('login/',auth_views.LoginView.as_view(), name='login'),
     path('admin/statuscheck/', include('celerybeat_status.urls')),
     path('logout/',auth_views.LogoutView.as_view(), name='logout'),
+    # path('log/', include('social_django.urls', namespace='social')),
     path('oauth/', include('social_django.urls', namespace='social')),
 
 
